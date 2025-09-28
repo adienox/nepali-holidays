20d4704ebd813b821c3db2aacd2a69e5d0161ac3
Adienox <adwait@adhk.dev>
2025-09-28 09:31:47 +0545

    fix!: update headers

--- parser.py
+++ parser.py
@@ -24,7 +24,10 @@ from icalendar import Calendar, Event
 URL = "https://en.wikipedia.org/wiki/Public_holidays_in_Nepal"
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 }
 
 
